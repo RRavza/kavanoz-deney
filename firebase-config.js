a7834c69c4f453c911f632108e48764c21b1b48e
RRavza <rabiaravzabaris2@gmail.com>
2026-09-24 13:30:07 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBkCHqB1yk717tya9VuA5eg7DF-fbZ1n-E",
+  authDomain: "kavanoz-deneyi.firebaseapp.com",
+  projectId: "kavanoz-deneyi",
+  storageBucket: "kavanoz-deneyi.firebasestorage.app",
+  messagingSenderId: "441519357843",
+  appId: "1:441519357843:web:f2cfec49451e0295427266"
+};
